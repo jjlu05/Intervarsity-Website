bcc0f6884f76c34e289fee48737feeee26098057
Jacob Lu <jacoblu2005@gmail.com>
2025-01-15 19:59:35 -0500

    nav bar and routes work, frontend is still ugly

--- StudentApp-starter-2024-main/app/forms.py
+++ StudentApp-starter-2024-main/app/forms.py
@@ -1,7 +1,24 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import  Length
 
-class CourseForm(FlaskForm):
-    coursenum = StringField('Course Number',[Length(min=3, max=6)])
-    submit = SubmitField('Post')
+from wtforms import FloatField, StringField, PasswordField, SubmitField, IntegerField, RadioField, BooleanField, TextAreaField, SelectField, ValidationError, FieldList, FormField
+from wtforms.validators import DataRequired, EqualTo, Email, Optional, Length, NumberRange
+
+from wtforms_sqlalchemy.fields import QuerySelectMultipleField
+from wtforms.widgets import ListWidget, CheckboxInput
+
+from app import db
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+
+    submit = SubmitField('Login')
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password', message="Passwords must match.")])
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name = StringField('Last Name', validators=[DataRequired()])
+    submit = SubmitField('Register')
+#NEED TO VALIDATE THAT EMAIL IS APART OF LEADERSHIP TEAM AND EACH EMAIL MUST BE UNIQUE
--- StudentApp-starter-2024-main/app/routes.py
+++ StudentApp-starter-2024-main/app/routes.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import render_template
-
+from app.forms import LoginForm, RegisterForm
 @app.route('/', methods=['GET'])
 @app.route('/index', methods=['GET'])
 def index():
@@ -11,3 +11,13 @@ def index():
 @app.route('/events', methods=['GET', 'POST'])
 def events():
     return render_template('events.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+   form = LoginForm()
+   return render_template('login.html', form = form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+   form = RegisterForm()
+   return render_template('register.html', form = form)
